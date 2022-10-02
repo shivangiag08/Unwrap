640251819215bac4144b03fd3fe0fe33e93edcc8
shivangiag08 <114857392+shivangiag08@users.noreply.github.com>
2022-10-02 18:54:14 +0530

    Update unwrap.py

--- unwrap.py
+++ unwrap.py
@@ -13,9 +13,6 @@ cam = VideoCapture(0)
 result, image = cam.read()
 if result:
     
-    # show the image
-    # imshow("GeeksForGeeks", image)
-    
     # save the image
     imwrite("scan.jpg", image)
 
@@ -24,11 +21,16 @@ if result:
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
 # determined by the first position in the shape tuple, in this case 1.
+
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
 # Replace this with the path to your image
+
 image = Image.open('scan.jpg')
+
 #resize the image to a 224x224 with the same strategy as in TM2:
 #resizing the image to be at least 224x224 and then cropping from the center
+
 size = (224, 224)
 image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
@@ -58,14 +60,3 @@ if d[max(l)] == 'Pringles':
 
 else:
     st.image('Pitchdeck2.jpg')
-
-
-# st.write("Here's the nutritional information for Pringles:")
-# st.write(pd.DataFrame({
-#     'first column': ['sugar', 2, 3, 4],
-#     'second column': ['Calories', 20, 30, 40]
-# }))
-# st.write()
-# st.write("Similar recommended products for you")
-# st.write("Lay's Sour  Cream and Onion Chips")
-# st.write("No milk and corn allergens")
